5cc07c0203f94d39bdf01326a43349ceb4dd60c4
Alexandre Dulaunoy <a@foo.be>
2016-03-14 20:40:06 +0100

    A minimal caching module added to cache link or url from MISP

---
+++ modules/expansion/sourcecache.py
@@ -0,0 +1,31 @@
+import json
+from url_archiver import url_archiver
+
+misperrors = {'error': 'Error'}
+mispattributes = {'input': ['link'], 'output': ['link']}
+moduleinfo = {'version': '0.1', 'author': 'Alexandre Dulaunoy', 'description': 'Module to cache web pages of analysis reports, OSINT sources. The module returns a link of the cached page.'}
+archive_path = '/tmp/'
+
+
+def handler(q=False):
+    if q is False:
+        return False
+    request = json.loads(q)
+    if request.get('link'):
+        tocache = request['link']
+        archiver = url_archiver.Archive(archive_path=archive_path)
+        archiver.fetch(url=tocache)
+        mispattributes['output'] = ['link']
+    else:
+        misperrors['error'] = "Link is missing"
+        return misperrors
+    r = {'results': [{'types': mispattributes['output'], 'values': tocache}]}
+    return r
+
+
+def introspection():
+    return mispattributes
+
+
+def version():
+    return moduleinfo
