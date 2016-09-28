a568d1a1b3aa992256b08b5e68c092c2e49080f3
Andreas Muehlemann <andreas.muehlemann@switch.ch>
2016-09-28 14:06:18 +0200

    updated geoip_country to __init__.py

--- misp_modules/modules/expansion/__init__.py
+++ misp_modules/modules/expansion/__init__.py
@@ -1,2 +1,3 @@
 __all__ = ['asn_history', 'circl_passivedns', 'circl_passivessl', 'countrycode', 'cve', 'dns',
-           'eupi', 'ipasn', 'passivetotal', 'sourcecache', 'virustotal', 'whois', 'shodan', 'reversedns']
+           'eupi', 'ipasn', 'passivetotal', 'sourcecache', 'virustotal', 'whois', 'shodan', 'reversedns'
+           'geoip_country']
