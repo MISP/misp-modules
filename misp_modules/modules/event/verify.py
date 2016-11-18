2b88f221306a92587df9c4cf1f0f95ec568b6f89
Raphaël Vinot <raphael@vinot.info>
2016-11-18 18:11:52 +0100

    Add sign/verify modules.

---
+++ misp_modules/modules/event/verify.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import json
+
+from pymisp import MISPEvent
+
+misperrors = {'error': 'Error'}
+
+moduleinfo = {'version': '0.1', 'author': 'Raphaël Vinot',
+              'description': 'Verify the signature of a MISP Event',
+              'module-type': ['event']}
+
+moduleconfig = ['uid']
+
+'''
+NOTE:
+* requires pyme3 + dependencies
+* working gpg-agent
+* the public key which signed the event in the keyring
+'''
+
+
+def handler(q=False):
+    if q is False:
+        return False
+    request = json.loads(q)  # Assuming request has two keys: config & mispevent (mispevent being the json dump of the event)
+    mispevent = MISPEvent()
+    mispevent.load(request['mispevent'])
+    mispevent.verify(mispevent.Org['uuid'])
+    # TODO: what do we return there?
+    # return json.dumps(mispevent, cls=EncodeUpdate)
+
+
+def introspection():
+    return moduleconfig
+
+
+def version():
+    moduleinfo['config'] = moduleconfig
+    return moduleinfo
