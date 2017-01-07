7a9774bff7c919805e9f064f3c5dc19e4b342a7f
Raphaël Vinot <raphael@vinot.info>
2017-01-06 19:23:23 -0500

    Add email_import in the modules loaded by default

--- misp_modules/modules/import_mod/__init__.py
+++ misp_modules/modules/import_mod/__init__.py
@@ -1,3 +1,3 @@
 from . import _vmray
 
-__all__ = ['vmray_import', 'testimport', 'ocr', 'stiximport', 'cuckooimport']
+__all__ = ['vmray_import', 'testimport', 'ocr', 'stiximport', 'cuckooimport', 'email_import']
