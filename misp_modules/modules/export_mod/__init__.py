5c4df3075e559d5357fd4613621b319d9525fc7a
Alexandre Dulaunoy <a@foo.be>
2018-01-08 20:31:26 +0100

    Fix the __init__ import

--- misp_modules/modules/export_mod/__init__.py
+++ misp_modules/modules/export_mod/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['testexport','cef_export','liteexport','threat_connect_export','pdfexport','threatStream_misp_export.py']
+__all__ = ['testexport','cef_export','liteexport','threat_connect_export','pdfexport','threatStream_misp_export']
