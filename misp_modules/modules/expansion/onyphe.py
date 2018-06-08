e6bac113baaf27981aa431f1f9c218bda49d0e17
Sebdraven <slarinier@gmail.com>
2018-06-08 16:38:41 +0200

    add onyphe module

---
+++ misp_modules/modules/expansion/onyphe.py
@@ -0,0 +1,65 @@
+import json
+# -*- coding: utf-8 -*-
+
+import json
+try:
+    from onyphe import Onyphe
+except ImportError:
+    print("pyonyphe module not installed.")
+
+misperrors = {'error': 'Error'}
+
+mispattributes = {'input': ['ip-src', 'ip-dst', 'hostname', 'domains'], 'output': ['freetext']}
+# possible module-types: 'expansion', 'hover' or both
+moduleinfo = {'version': '1', 'author': 'Sebastien Larinier @sebdraven',
+              'description': 'Query on Onyphe',
+              'module-type': ['expansion', 'hover']}
+
+# config fields that your code expects from the site admin
+moduleconfig = ['apikey']
+
+
+
+def handler(q=False):
+    if q is False:
+        return False
+    request = json.loads(q)
+
+    if not request.get('config') and not (request['config'].get('apikey')):
+        misperrors['error'] = 'Onyphe authentication is missing'
+        return misperrors
+
+    api = Onyphe(request['config'].get('apikey'))
+
+    if not api:
+        misperrors['error'] = 'Onyphe Error instance api'
+
+    ip = ''
+    if request.get('ip-src'):
+        ip = request['ip-src']
+    elif request.get('ip-dst'):
+        ip = request['ip-dst']
+    else:
+        misperrors['error'] = "Unsupported attributes type"
+        return misperrors
+
+    return handle_expansion(api, ip, misperrors)
+
+
+def handle_expansion(api, ip, misperrors):
+    result = api.ip(ip)
+    if result['status'] == 'nok':
+        misperrors['error'] = result['message']
+        return misperrors
+
+    return {'results': [{'types': mispattributes['output'],
+                         'values': json.dumps(result)}]}
+
+
+def introspection():
+    return mispattributes
+
+
+def version():
+    moduleinfo['config'] = moduleconfig
+    return moduleinfo
