7171c8ce92432e7850b8f5d6a51420c3543a36b0
Sascha Rommelfangen <sascha@rommelfangen.de>
2019-04-24 13:54:21 +0200

    initial version of OCR expansion module

--- misp_modules/modules/expansion/__init__.py
+++ misp_modules/modules/expansion/__init__.py
@@ -8,4 +8,5 @@ __all__ = ['cuckoo_submit', 'vmray_submit', 'bgpranking', 'circl_passivedns', 'c
            'yara_syntax_validator', 'hashdd', 'onyphe', 'onyphe_full', 'rbl',
            'xforceexchange', 'sigma_syntax_validator', 'stix2_pattern_syntax_validator',
            'sigma_queries', 'dbl_spamhaus', 'vulners', 'yara_query', 'macaddress_io',
-           'intel471', 'backscatter_io', 'btc_scam_check', 'hibp', 'greynoise', 'macvendors', 'qrcode']
+           'intel471', 'backscatter_io', 'btc_scam_check', 'hibp', 'greynoise', 'macvendors', 
+           'qrcode', 'ocr']
