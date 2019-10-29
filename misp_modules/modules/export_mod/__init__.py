8ac4b610b8353ba99282b9e1f715d32e8b5e6d9d
Braden Laverick <blav62@gmail.com>
2019-10-29 15:11:31 +0000

    Added endgame export to __all__

--- misp_modules/modules/export_mod/__init__.py
+++ misp_modules/modules/export_mod/__init__.py
@@ -1,2 +1,2 @@
-__all__ = ['cef_export', 'liteexport', 'goamlexport', 'threat_connect_export', 'pdfexport',
+__all__ = ['cef_export', 'liteexport', 'goamlexport', 'endgame_export', 'threat_connect_export', 'pdfexport',
            'threatStream_misp_export', 'osqueryexport', 'nexthinkexport']
