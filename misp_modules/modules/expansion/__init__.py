7945d060ff11bfd538590fdc01329ef56bfcbeba
chrisr3d <chris.studer.68@gmail.com>
2019-12-18 17:11:13 +0100

    new: Enrichment module for querying APIVoid with domain attributes

--- misp_modules/modules/expansion/__init__.py
+++ misp_modules/modules/expansion/__init__.py
@@ -14,6 +14,6 @@ __all__ = ['cuckoo_submit', 'vmray_submit', 'bgpranking', 'circl_passivedns', 'c
            'intel471', 'backscatter_io', 'btc_scam_check', 'hibp', 'greynoise', 'macvendors',
            'qrcode', 'ocr_enrich', 'pdf_enrich', 'docx_enrich', 'xlsx_enrich', 'pptx_enrich',
            'ods_enrich', 'odt_enrich', 'joesandbox_submit', 'joesandbox_query', 'urlhaus',
-           'virustotal_public', 'apiosintds', 'urlscan', 'securitytrails',
+           'virustotal_public', 'apiosintds', 'urlscan', 'securitytrails', 'apivoid',
            'assemblyline_submit', 'assemblyline_query', 'ransomcoindb',
            'lastline_query', 'lastline_submit']
