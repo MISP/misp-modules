c9c6f69bd432e2a98129d02c3ad2b125fb321a80
chrisr3d <chris.studer.68@gmail.com>
2020-02-26 11:59:14 +0100

    fix: Making pep8 happy

--- misp_modules/modules/expansion/google_search.py
+++ misp_modules/modules/expansion/google_search.py
@@ -1,37 +1,35 @@
 import json
 import requests
 try:
-	from google import google
+    from google import google
 except ImportError:
-	print("GoogleAPI not installed. Command : pip install git+https://github.com/abenassi/Google-Search-API")
+    print("GoogleAPI not installed. Command : pip install git+https://github.com/abenassi/Google-Search-API")
 
 misperrors = {'error': 'Error'}
 mispattributes = {'input': ['url'], 'output': ['text']}
-moduleinfo = {'author': 'Oun & Gindt', 'description': 'An expansion hover module to expand google search information about an URL', 'module-type': ['hover']}
+moduleinfo = {'author': 'Oun & Gindt', 'module-type': ['hover'],
+              'description': 'An expansion hover module to expand google search information about an URL'}
 
 
 def handler(q=False):
-	if q is False:
-		return False
-	request = json.loads(q)
-	if request.get('url'):
-		toquery = request['url']
-	else:
-		misperrors['error'] = "Unsupported attributes type"
-		return misperrors
+    if q is False:
+        return False
+    request = json.loads(q)
+    if not request.get('url'):
+        return {'error': "Unsupported attributes type"}
+    num_page = 1
+    res = ""
+    search_results = google.search(request['url'], num_page)
+    for i in range(3):
+        res += "("+str(i+1)+")" + '\t'
+        res += json.dumps(search_results[i].description, ensure_ascii=False)
+        res += '\n\n'
+    return {'results': [{'types': mispattributes['output'], 'values':res}]}
 
-	num_page = 1
-	res = ""
-	search_results = google.search(request['url'], num_page)
-	for i in range(3):
-		res += "("+str(i+1)+")" + '\t'
-		res += json.dumps(search_results[i].description, ensure_ascii=False)
-		res += '\n\n'
-	return {'results': [{'types': mispattributes['output'], 'values':res}]}
 
 def introspection():
-	return mispattributes
+    return mispattributes
 
 
 def version():
-	return moduleinfo
+    return moduleinfo
