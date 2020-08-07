MISP/misp-modules

commit 3b7a5c4dc2541f3b07baee69a7e8b9694a1627fc

add: Specific error message for misp_standard format expansion modules
- Checking if the input format is respected and
  displaying an error message if it is not

misp_modules/modules/expansion/recordedfuture.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import requests
+from . import check_input_attribute, checking_error, standard_error_message
 from urllib.parse import quote
 from pymisp import MISPAttribute, MISPEvent, MISPTag, MISPObject
 
@@ -257,6 +258,10 @@ def handler(q=False):
     else:
         misperrors['error'] = 'Missing Recorded Future token.'
         return misperrors
+    if not request.get('attribute') or not check_input_attribute(request['atttribute'], requirements=('type', 'value')):
+        return {'error': f'{standard_error_message}, {checking_error}.'}
+    if request['attribute']['type'] not in mispattributes['input']:
+        return {'error': 'Unsupported attribute type.'}
 
     input_attribute = request.get('attribute')
     rf_enricher = RFEnricher(token, input_attribute)

misp_modules/modules/expansion/__init__.py
@@ -19,3 +19,12 @@ __all__ = ['cuckoo_submit', 'vmray_submit', 'bgpranking', 'circl_passivedns', 'c
            'assemblyline_submit', 'assemblyline_query', 'ransomcoindb', 'malwarebazaar',
            'lastline_query', 'lastline_submit', 'sophoslabs_intelix', 'cytomic_orion', 'censys_enrich',
            'trustar_enrich', 'recordedfuture']
+
+
+minimum_required_fields = ('type', 'uuid', 'value')
+
+checking_error = 'containing at least a "type" field and a "value" field'
+standard_error_message = 'This module requires an "attribute" field as input'
+
+def check_input_attribute(attribute, requirements=minimum_required_fields):
+    return all(feature in attribute for feature in requirements)

misp_modules/modules/expansion/apivoid.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from . import check_input_attribute, standard_error_message
 from pymisp import MISPAttribute, MISPEvent, MISPObject
 
 misperrors = {'error': 'Error'}
@@ -74,7 +75,11 @@ def handler(q=False):
     request = json.loads(q)
     if not request.get('config', {}).get('apikey'):
         return {'error': 'An API key for APIVoid is required.'}
-    attribute = request.get('attribute')
+    if not request.get('attribute') or not check_input_attribute(request['attribute']):
+        return {'error': f'{standard_error_message}, which should contain at least a type, a value and an uuid.'}
+    attribute = request['attribute']
+    if attribute['type'] not in mispattributes['input']:
+        return {'error': 'Unsupported attribute type.'}
     apikey = request['config']['apikey']
     apivoid_parser = APIVoidParser(attribute)
     apivoid_parser.parse_domain(apikey)

misp_modules/modules/expansion/assemblyline_query.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import json
+from . import check_input_attribute, standard_error_message
 from assemblyline_client import Client, ClientError
 from collections import defaultdict
 from pymisp import MISPAttribute, MISPEvent, MISPObject
@@ -139,6 +140,10 @@ def handler(q=False):
     if q is False:
         return False
     request = json.loads(q)
+    if not request.get('attribute') or not check_input_attribute(request['attribute']):
+        return {'error': f'{standard_error_message}, which should contain at least a type, a value and an uuid.'}
+    if request['attribute']['type'] not in mispattributes['input']:
+        return {'error': 'Unsupported attribute type.'}
     if not request.get('config'):
         return {"error": "Missing configuration."}
     if not request['config'].get('apiurl'):

misp_modules/modules/expansion/censys_enrich.py
@@ -3,6 +3,7 @@ import json
 import base64
 import codecs
 from dateutil.parser import isoparse
+from . import check_input_attribute, standard_error_message
 from pymisp import MISPAttribute, MISPEvent, MISPObject
 try:
     import censys.base
@@ -36,11 +37,11 @@ def handler(q=False):
     api_id = request['config']['api_id']
     api_secret = request['config']['api_secret']
 
-    if not request.get('attribute'):
-        return {'error': 'Unsupported input.'}
+    if not request.get('attribute') or not check_input_attribute(request['attribute']):
+        return {'error': f'{standard_error_message}, which should contain at least a type, a value and an uuid.'}
     attribute = request['attribute']
     if not any(input_type == attribute['type'] for input_type in mispattributes['input']):
-        return {'error': 'Unsupported attributes type'}
+        return {'error': 'Unsupported attribute type.'}
 
     attribute = MISPAttribute()
     attribute.from_dict(**request['attribute'])

misp_modules/modules/expansion/circl_passivedns.py
@@ -1,5 +1,6 @@
 import json
 import pypdns
+from . import check_input_attribute, standard_error_message
 from pymisp import MISPAttribute, MISPEvent, MISPObject
 
 mispattributes = {'input': ['hostname', 'domain', 'ip-src', 'ip-dst', 'ip-src|port', 'ip-dst|port'], 'format': 'misp_standard'}
@@ -58,11 +59,11 @@ def handler(q=False):
     if not request['config'].get('username') or not request['config'].get('password'):
         return {'error': 'CIRCL Passive DNS authentication is incomplete, please provide your username and password.'}
     authentication = (request['config']['username'], request['config']['password'])
-    if not request.get('attribute'):
-        return {'error': 'Unsupported input.'}
+    if not request.get('attribute') or not check_input_attribute(request['attribute']):
+        return {'error': f'{standard_error_message}, which should contain at least a type, a value and an uuid.'}
     attribute = request['attribute']
     if not any(input_type == attribute['type'] for input_type in mispattributes['input']):
-        return {'error': 'Unsupported attributes type'}
+        return {'error': 'Unsupported attribute type.'}
     pdns_parser = PassiveDNSParser(attribute, authentication)
     pdns_parser.parse()
     return pdns_parser.get_results()

misp_modules/modules/expansion/circl_passivessl.py
@@ -1,5 +1,6 @@
 import json
 import pypssl
+from . import check_input_attribute, standard_error_message
 from pymisp import MISPAttribute, MISPEvent, MISPObject
 
 mispattributes = {'input': ['ip-src', 'ip-dst', 'ip-src|port', 'ip-dst|port'], 'format': 'misp_standard'}
@@ -83,11 +84,11 @@ def handler(q=False):
     if not request['config'].get('username') or not request['config'].get('password'):
         return {'error': 'CIRCL Passive SSL authentication is incomplete, please provide your username and password.'}
     authentication = (request['config']['username'], request['config']['password'])
-    if not request.get('attribute'):
-        return {'error': 'Unsupported input.'}
+    if not request.get('attribute') or not check_input_attribute(request['attribute']):
+        return {'error': f'{standard_error_message}, which should contain at least a type, a value and an uuid.'}
     attribute = request['attribute']
     if not any(input_type == attribute['type'] for input_type in mispattributes['input']):
-        return {'error': 'Unsupported attributes type'}
+        return {'error': 'Unsupported attribute type.'}
     pssl_parser = PassiveSSLParser(attribute, authentication)
     pssl_parser.parse()
     return pssl_parser.get_results()

misp_modules/modules/expansion/cve_advanced.py
@@ -1,7 +1,8 @@
-from collections import defaultdict
-from pymisp import MISPEvent, MISPObject
 import json
 import requests
+from . import check_input_attribute, standard_error_message
+from collections import defaultdict
+from pymisp import MISPEvent, MISPObject
 
 misperrors = {'error': 'Error'}
 mispattributes = {'input': ['vulnerability'], 'format': 'misp_standard'}
@@ -108,7 +109,8 @@ def handler(q=False):
     if q is False:
         return False
     request = json.loads(q)
-    attribute = request.get('attribute')
+    if not request.get('attribute') or not check_input_attribute(request['attribute']):
+        return {'error': f'{standard_error_message}, which should contain at least a type, a value and an uuid.'}
     if attribute.get('type') != 'vulnerability':
         misperrors['error'] = 'Vulnerability id missing.'
         return misperrors

commit 85d319e85e1e53349685b3124aa13b020891b382

Fix typo error introduced in commit: 3b7a5c4dc2541f3b07baee69a7e8b9694a1627fc

misp_modules/modules/expansion/recordedfuture.py
@@ -258,7 +258,7 @@ def handler(q=False):
     else:
         misperrors['error'] = 'Missing Recorded Future token.'
         return misperrors
-    if not request.get('attribute') or not check_input_attribute(request['atttribute'], requirements=('type', 'value')):
+    if not request.get('attribute') or not check_input_attribute(request['attribute'], requirements=('type', 'value')):
         return {'error': f'{standard_error_message}, {checking_error}.'}
     if request['attribute']['type'] not in mispattributes['input']:
         return {'error': 'Unsupported attribute type.'}