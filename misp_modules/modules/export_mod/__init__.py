451531326d18a4b092a15778598529ad77f91043
milkmix <julien.bachmann@gmail.com>
2020-11-20 16:29:08 +0100

    initial work on Defender for Endpoint export module

--- misp_modules/modules/export_mod/__init__.py
+++ misp_modules/modules/export_mod/__init__.py
@@ -1,2 +1,2 @@
 __all__ = ['cef_export', 'mass_eql_export', 'liteexport', 'goamlexport', 'threat_connect_export', 'pdfexport',
-           'threatStream_misp_export', 'osqueryexport', 'nexthinkexport', 'vt_graph']
+           'threatStream_misp_export', 'osqueryexport', 'nexthinkexport', 'vt_graph', 'defender_endpoint']
