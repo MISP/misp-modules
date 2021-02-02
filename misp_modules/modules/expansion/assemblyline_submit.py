07b8968b7d65afcec886607eddef421e530f8155
adammchugh <34958726+adammchugh@users.noreply.github.com>
2021-02-02 22:52:27 +1030

    Update assemblyline_submit.py

--- misp_modules/modules/expansion/assemblyline_submit.py
+++ misp_modules/modules/expansion/assemblyline_submit.py
@@ -7,7 +7,7 @@ from urllib.parse import urljoin
 
 moduleinfo = {"version": 1, "author": "Christian Studer", "module-type": ["expansion"],
               "description": "Submit files or URLs to AssemblyLine"}
-moduleconfig = ["apiurl", "user_id", "apikey", "password"]
+moduleconfig = ["apiurl", "user_id", "apikey", "password", "verifyssl"]
 mispattributes = {"input": ["attachment", "malware-sample", "url"],
                   "output": ["link"]}
 
