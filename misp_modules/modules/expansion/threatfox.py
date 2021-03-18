458e432bb7e0948b83b607bef81306c7ba369488
chrisr3d <chris.studer.68@gmail.com>
2021-03-18 19:22:26 +0100

    fix: Making pep8 happy

--- misp_modules/modules/expansion/threatfox.py
+++ misp_modules/modules/expansion/threatfox.py
@@ -3,11 +3,12 @@ import requests
 import json
 
 misperrors = {'error': 'Error'}
-mispattributes = {'input': ['md5', 'sha1', 'sha256', 'domain', 'url', 'email-src', 'ip-dst|port', 'ip-src|port'],'output': ['text']}
+mispattributes = {'input': ['md5', 'sha1', 'sha256', 'domain', 'url', 'email-src', 'ip-dst|port', 'ip-src|port'], 'output': ['text']}
 moduleinfo = {'version': '0.1', 'author': 'Corsin Camichel', 'description': 'Module to search for an IOC on ThreatFox by abuse.ch.', 'module-type': ['hover', 'expansion']}
 moduleconfig = []
 
 API_URL = "https://threatfox-api.abuse.ch/api/v1/"
+
 
 # copied from
 # https://github.com/marjatech/threatfox2misp/blob/main/threatfox2misp.py
@@ -26,6 +27,7 @@ def confidence_level_to_tag(level: int) -> str:
             confidence_tag = tag
     return confidence_tag
 
+
 def handler(q=False):
     if q is False:
         return False
@@ -41,18 +43,20 @@ def handler(q=False):
         misperrors['error'] = "Unsupported attributes type:"
         return misperrors
 
-    data = { "query": "search_ioc", "search_term": f"{to_query}" }
+    data = {"query": "search_ioc", "search_term": f"{to_query}"}
     response = requests.post(API_URL, data=json.dumps(data))
     if response.status_code == 200:
         result = json.loads(response.text)
         if(result["query_status"] == "ok"):
             confidence_tag = confidence_level_to_tag(result["data"][0]["confidence_level"])
-            ret_val = {'results': [{'types': mispattributes['output'], 'values': [result["data"][0]["threat_type_desc"]], 'tags': [result["data"][0]["malware"], result["data"][0]["malware_printable"], confidence_tag ] }]}
+            ret_val = {'results': [{'types': mispattributes['output'], 'values': [result["data"][0]["threat_type_desc"]], 'tags': [result["data"][0]["malware"], result["data"][0]["malware_printable"], confidence_tag]}]}
 
     return ret_val
 
+
 def introspection():
     return mispattributes
+
 
 def version():
     moduleinfo['config'] = moduleconfig
