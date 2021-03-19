9eb41f4022a16fc87a7137386491c576f7ae2ce4
Sebdraven <sebdraven@protonmail.com>
2021-03-19 14:26:44 +0100

    Update yeti.py
    change relation type

--- misp_modules/modules/expansion/yeti.py
+++ misp_modules/modules/expansion/yeti.py
@@ -93,14 +93,14 @@ class Yeti():
         typ_attribute = self.misp_mapping[obj_yeti['type']]
         attr_misp = {'type': typ_attribute, 'value': obj_yeti['value']}
         if typ_attribute == 'ip-src' or typ_attribute == 'ip-dst':
-            attr_misp['object_relation'] = {'type': 'text',
-                                            'object_relation': 'ip'}
+            attr_misp['object_relation'].update({'type': 'text',
+                                                 'object_relation': 'ip'})
         elif 'domain' == typ_attribute:
-            attr_misp['object_relation'] = {'type': 'text',
-                                            'object_relation': 'domain'}
+            attr_misp.update({'type': 'text',
+                              'object_relation': 'domain'})
         elif 'hostname' == typ_attribute:
-            attr_misp['object_relation'] = {'type': 'text',
-                                            'object_relation': 'domain'}
+            attr_misp.update({'type': 'text',
+                              'object_relation': 'domain'})
             attr_misp['type'] = 'domain'
         else:
             attr_misp['object_relation'] = None
