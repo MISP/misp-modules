be212097a759cb6e39a2ca763921dc144b8f19a1
Sebdraven <sebdraven@protonmail.com>
2021-04-19 11:08:21 +0200

    Update yeti.py
    add log

--- misp_modules/modules/expansion/yeti.py
+++ misp_modules/modules/expansion/yeti.py
@@ -112,6 +112,7 @@ class Yeti():
             type_attribute = self.misp_mapping[obj['type']]
         else:
             type_attribute = obj['type']
+        print(type_attribute)
         if type_attribute == 'ip-src' or type_attribute == 'ip-dst':
             return 'ip'
         elif 'domain' == type_attribute:
