dc0660acd0df74e874be4cf81c9b5d4381fb594f
Calvin Krzywiec <ckrzywiec@qintel.com>
2021-11-22 15:46:46 -0500

    feature: add qintel qsentry expansion module

--- misp_modules/modules/expansion/__init__.py
+++ misp_modules/modules/expansion/__init__.py
@@ -17,7 +17,8 @@ __all__ = ['cuckoo_submit', 'vmray_submit', 'bgpranking', 'circl_passivedns', 'c
            'virustotal_public', 'apiosintds', 'urlscan', 'securitytrails', 'apivoid',
            'assemblyline_submit', 'assemblyline_query', 'ransomcoindb', 'malwarebazaar',
            'lastline_query', 'lastline_submit', 'sophoslabs_intelix', 'cytomic_orion', 'censys_enrich',
-           'trustar_enrich', 'recordedfuture', 'html_to_markdown', 'socialscan', 'passive-ssh']
+           'trustar_enrich', 'recordedfuture', 'html_to_markdown', 'socialscan', 'passive-ssh',
+           'qintel_qsentry']
 
 
 minimum_required_fields = ('type', 'uuid', 'value')
