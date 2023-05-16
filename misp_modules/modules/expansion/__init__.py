ddd8b8513eaff8dac047c5f13f160a25f9827373
Christian Studer <christian.studer@circl.lu>
2023-05-16 16:09:04 +0200

    add: [expansion modules] Added `ipinfo` to the expansion modules list in `__init__`

--- misp_modules/modules/expansion/__init__.py
+++ misp_modules/modules/expansion/__init__.py
@@ -19,7 +19,8 @@ __all__ = ['cuckoo_submit', 'vmray_submit', 'bgpranking', 'circl_passivedns', 'c
            'lastline_query', 'lastline_submit', 'sophoslabs_intelix', 'cytomic_orion', 'censys_enrich',
            'trustar_enrich', 'recordedfuture', 'html_to_markdown', 'socialscan', 'passive-ssh',
            'qintel_qsentry', 'mwdb', 'hashlookup', 'mmdb_lookup', 'ipqs_fraud_and_risk_scoring',
-           'clamav', 'jinja_template_rendering','hyasinsight', 'variotdbs', 'crowdsec', 'extract_url_components']
+           'clamav', 'jinja_template_rendering','hyasinsight', 'variotdbs', 'crowdsec',
+           'extract_url_components', 'ipinfo']
 
 
 minimum_required_fields = ('type', 'uuid', 'value')
