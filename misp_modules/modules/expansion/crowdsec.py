6eea5f61d4cb20cb6b3c36c20b48ed10704d0169
Christian Studer <christian.studer@circl.lu>
2023-05-23 13:20:52 +0200

    fix: [crowdsec] Fixed the `reverse_dns` field parsing & added the `background-noise` attribute

--- misp_modules/modules/expansion/crowdsec.py
+++ misp_modules/modules/expansion/crowdsec.py
@@ -70,7 +70,9 @@ def _handler_v2(request_data):
     crowdsec_context_object.add_attribute("longitude", crowdsec_cti["location"]["longitude"])
     crowdsec_context_object.add_attribute("as-name", crowdsec_cti["as_name"])
     crowdsec_context_object.add_attribute("as-num", crowdsec_cti["as_num"])
-    crowdsec_context_object.add_attribute("reverse-dns", crowdsec_cti["reverse_dns"])
+    if crowdsec_cti.get('reverse_dns') is not None:
+        crowdsec_context_object.add_attribute("reverse-dns", crowdsec_cti["reverse_dns"])
+    crowdsec_context_object.add_attribute('background-noise', crowdsec_cti['background_noise_score']
     for behavior in crowdsec_cti["behaviors"]:
         crowdsec_context_object.add_attribute(
             "behaviors", behavior["label"],
